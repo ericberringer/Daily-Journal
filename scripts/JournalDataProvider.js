744c73a68537da54d22ae81f607555a7b2a7f249
ericberringer <eric.berringer@gmail.com>
2021-02-17 16:09:15 -0600

    updated the journaldataprovider.js to access the new api (entries.json). Is not printing to the DOM, that needs to be fixed.

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,31 +1,14 @@
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Happy"
-    },
-    {
-        id: 2,
-        date: "05/09/2021",
-        concept: "Death by Code",
-        entry: "Learned that the term 'Learn to Code' does not quite encompass the amount of work and difficulty involved with learning this new skill.",
-        mood: "Angry"
-    },
-    {
-        id: 3,
-        date: "07/24/2025",
-        concept: "Javascript",
-        entry: "Javascript makes me want to die.",
-        mood: "Indifferent"
-    }
-]
+let entries = []
 
-export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
+export const useEntries = () => {
+    return entries.slice()
 }
+
+export const getEntries = () => {
+    return fetch("http://localhost:8088/entries") // Fetch from the API
+        .then(response => response.json())  // Parse as JSON
+        .then(parsedEntries => entries = parsedEntries)
+            // What should happen when we finally have the array?
+}
+
+console.log(getEntries())
